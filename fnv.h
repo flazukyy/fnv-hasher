2b69c781473b63166a09c68a1eb7f5467a6641cc
flazukyy <flazuky.science@gmail.com>
2026-04-18 12:52:46 -0700

    update

--- fnv.h
+++ fnv.h
@@ -2,16 +2,15 @@
 #include <cstdint>
 #include <string_view>
 
-using hash_t = unsigned;
-
 namespace fnv {
+	using hash_t = unsigned;
+
 	inline constexpr hash_t FNV_BASE{ 0x384d8f3 };
 	inline constexpr hash_t FNV_OFFSET{ 0x9d4930 };
 
 	[[nodiscard]] hash_t hash(std::string_view str) noexcept {
 		hash_t _hash{ FNV_BASE };
 		for (unsigned char _char : str) {
-			if (_char == '\0') return _hash;
 			_hash *= FNV_OFFSET;
 			_hash ^= _char;
 		}
@@ -22,7 +21,6 @@ namespace fnv {
 	[[nodiscard]] consteval hash_t hash_const(std::string_view str) noexcept {
 		hash_t _hash{ FNV_BASE };
 		for (unsigned char _char : str) {
-			if (_char == '\0') return _hash;
 			_hash *= FNV_OFFSET;
 			_hash ^= _char;
 		}
@@ -34,7 +32,6 @@ namespace fnv {
 		[[nodiscard]] hash_t hash(std::string_view str) noexcept {
 			hash_t _hash{ FNV_BASE };
 			for (unsigned char _char : str) {
-				if (_char == '\0') return _hash;
 				_hash ^= _char;
 				_hash *= FNV_OFFSET;
 			}
@@ -45,7 +42,6 @@ namespace fnv {
 		[[nodiscard]] consteval hash_t hash_const(std::string_view str) noexcept {
 			hash_t _hash{ FNV_BASE };
 			for (unsigned char _char : str) {
-				if (_char == '\0') return _hash;
 				_hash ^= _char;
 				_hash *= FNV_OFFSET;
 			}
