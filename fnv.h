756f16f08e9b8794c62bf7145e37d221eb0788d7
flazukyy <flazuky.science@gmail.com>
2026-04-20 12:40:43 -0600

    64 bit hashing
    might add more hash types later and make this a generic hasher

--- fnv.h
+++ fnv.h
@@ -3,25 +3,48 @@
 #include <string_view>
 
 namespace fnv {
-	using hash_t = unsigned;
+	using hash32_t = unsigned;
+	using hash64_t = long long;
 
-	inline constexpr hash_t FNV_BASE{ 0x384d8f3 };
-	inline constexpr hash_t FNV_OFFSET{ 0x9d4930 };
+	inline constexpr hash32_t FNV32_BASE{ 0x384d8f3 };
+	inline constexpr hash32_t FNV32_OFFSET{ 0x9d4930 };
+	inline constexpr hash64_t FNV64_BASE{ 0x384d8f3399827c7c };
+	inline constexpr hash64_t FNV64_OFFSET{ 0x9d493034d9430c };
 
-	[[nodiscard]] hash_t hash(std::string_view str) noexcept {
-		hash_t _hash{ FNV_BASE };
+	[[nodiscard]] hash32_t hash(std::string_view str) noexcept {
+		hash32_t _hash{ FNV32_BASE };
 		for (unsigned char _char : str) {
-			_hash *= FNV_OFFSET;
+			_hash *= FNV32_OFFSET;
 			_hash ^= _char;
 		}
 
 		return _hash;
 	}
 
-	[[nodiscard]] consteval hash_t hash_const(std::string_view str) noexcept {
-		hash_t _hash{ FNV_BASE };
+	[[nodiscard]] consteval hash32_t hash_const(std::string_view str) noexcept {
+		hash32_t _hash{ FNV32_BASE };
 		for (unsigned char _char : str) {
-			_hash *= FNV_OFFSET;
+			_hash *= FNV32_OFFSET;
+			_hash ^= _char;
+		}
+
+		return _hash;
+	}
+
+	[[nodiscard]] hash64_t hash64(std::string_view str) noexcept {
+		hash64_t _hash{ FNV64_BASE };
+		for (unsigned char _char : str) {
+			_hash *= FNV64_OFFSET;
+			_hash ^= _char;
+		}
+
+		return _hash;
+	}
+
+	[[nodiscard]] consteval hash64_t hash64_const(std::string_view str) noexcept {
+		hash64_t _hash{ FNV64_BASE };
+		for (unsigned char _char : str) {
+			_hash *= FNV64_OFFSET;
 			_hash ^= _char;
 		}
 
@@ -29,21 +52,41 @@ namespace fnv {
 	}
 
 	namespace a1 {
-		[[nodiscard]] hash_t hash(std::string_view str) noexcept {
-			hash_t _hash{ FNV_BASE };
+		[[nodiscard]] hash32_t hash(std::string_view str) noexcept {
+			hash32_t _hash{ FNV32_BASE };
 			for (unsigned char _char : str) {
 				_hash ^= _char;
-				_hash *= FNV_OFFSET;
+				_hash *= FNV32_OFFSET;
 			}
 
 			return _hash;
 		}
 
-		[[nodiscard]] consteval hash_t hash_const(std::string_view str) noexcept {
-			hash_t _hash{ FNV_BASE };
+		[[nodiscard]] consteval hash32_t hash_const(std::string_view str) noexcept {
+			hash32_t _hash{ FNV32_BASE };
 			for (unsigned char _char : str) {
 				_hash ^= _char;
-				_hash *= FNV_OFFSET;
+				_hash *= FNV32_OFFSET;
+			}
+
+			return _hash;
+		}
+
+		[[nodiscard]] hash64_t hash64(std::string_view str) noexcept {
+			hash64_t _hash{ FNV64_BASE };
+			for (unsigned char _char : str) {
+				_hash ^= _char;
+				_hash *= FNV64_OFFSET;
+			}
+
+			return _hash;
+		}
+
+		[[nodiscard]] consteval hash64_t hash64_const(std::string_view str) noexcept {
+			hash64_t _hash{ FNV64_BASE };
+			for (unsigned char _char : str) {
+				_hash ^= _char;
+				_hash *= FNV64_OFFSET;
 			}
 
 			return _hash;
